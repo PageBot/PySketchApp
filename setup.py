0b3579d001d006af389ebb7fcef5d2afc193c21a
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-01-14 12:14:03 +0100

    Installable package.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sketchapp2py',
+    version="0.1",
+    description="A Pythonic wrapper around Sketch.",
+    author="Petr van Blokland",
+    author_email="buro@petr.com",
+    url="https://github.com/PageBot/SketchApp2Py.git",
+    license="MIT",
+    packages=["sketchapp2py"],
+    package_dir={"": "Lib"}
+)
